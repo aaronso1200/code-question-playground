10bb1e434fe14c8805f41e3c8a76d85925463d7d
ESDLIFE\Aarons <aaronso1200@gmail.com>
2021-10-19 17:55:55 +0800

    Add Solution: Invert tree

---
+++ inverseTree/inverseTree.js
@@ -0,0 +1,28 @@
+/**
+ * Definition for a binary tree node.
+ */
+  function TreeNode(val, left, right) {
+      this.val = (val===undefined ? 0 : val)
+      this.left = (left===undefined ? null : left)
+      this.right = (right===undefined ? null : right)
+  }
+/**
+ * @param {TreeNode} root
+ * @return {TreeNode}
+ */
+var invertTree = function(root) {
+    if (!root) {
+        return root
+    }
+    let temp = root.left
+    invertTree(root.left)
+    invertTree(root.right)
+    root.left = root.right
+    root.right = temp
+    return root
+};
+
+module.exports = {
+    TreeNode,
+    invertTree
+}

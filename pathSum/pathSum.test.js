955ab363646af877f7f0ddaab9f7699edd9c667e
ESDLIFE\Aarons <aaronso1200@gmail.com>
2021-10-22 12:16:36 +0800

    Path Sum Solution

--- pathSum/pathSum.test.js
+++ pathSum/pathSum.test.js
@@ -2,7 +2,7 @@ const hasPathSum = require('./pathSum').hasPathSum
 const TreeNode = require('./pathSum').TreeNode
 
 describe('Has Path Sum Function',()=> {
-    test('Has Path Sum should return true',()=> {
+    test('if has Path Sum should return true',()=> {
         let input = new TreeNode(4)
         input.left = new TreeNode(2)
         input.right = new TreeNode(7)
@@ -22,5 +22,16 @@ describe('Has Path Sum Function',()=> {
         input.right.right = new TreeNode(9)
         input.right.left = new TreeNode(6)
         expect(hasPathSum(input,999)).toEqual(false)
+    })
+
+    test('if root node right does not exist should return false',()=> {
+        let input = new TreeNode(4)
+        input.left = new TreeNode(2)
+        expect(hasPathSum(input,4)).toEqual(false)
+    })
+
+    test('if only exist root node should return true',()=>{
+        let input = new TreeNode(4)
+        expect(hasPathSum(input,4)).toEqual(true)
     })
 })

8545be0b654e4722af9430e2140c2d20646185dd
ESDLIFE\Aarons <aaronso1200@gmail.com>
2022-02-11 16:46:54 +0800

    Add test case: excel sheet title

--- [171]excelSheetTitle/excelSheetTitle.test.js
+++ [171]excelSheetTitle/excelSheetTitle.test.js
@@ -3,7 +3,12 @@ const {titleToNumber} = require('./excelSheetTitle')
 describe('test', function () {
     test('test',() => {})
        expect(titleToNumber("A")).toEqual(1);
+       expect(titleToNumber("Z")).toEqual(26);
        expect(titleToNumber("AB")).toEqual(28);
+       expect(titleToNumber("AAA")).toEqual(703);
+       expect(titleToNumber("ABC")).toEqual(731);
+       expect(titleToNumber("AZ")).toEqual(52);
+       expect(titleToNumber("BA")).toEqual(53);
        expect(titleToNumber("ZY")).toEqual(701);
        expect(titleToNumber("FXSHRXW")).toEqual(2147483647 );
 });

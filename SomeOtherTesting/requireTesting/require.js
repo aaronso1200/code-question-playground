ac62fa99dca995a59f4082fe02e2467fc24a914e
ESDLIFE\Aarons <aaronso1200@gmail.com>
2022-04-12 10:31:27 +0800

    Add Testing for require

---
+++ SomeOtherTesting/requireTesting/require.js
@@ -0,0 +1,18 @@
+let a = require('./a.json');
+let a2 = require('./a.json')
+const name = `./a.json`
+let a4 = require(name)
+
+delete require.cache[require.resolve('./a.json')]
+let a3 = require('./a.json')
+
+console.log("A=A2?");
+console.log( a===a2);
+
+console.log("A=A3?");
+console.log(a===a3);
+
+console.log("A2=A3?");
+console.log(a2===a3);
+
+console.log(a4===a2)

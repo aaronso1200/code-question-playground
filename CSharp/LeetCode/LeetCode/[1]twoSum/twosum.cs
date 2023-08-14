97996295c40bdcbfcda432fe852d03c5493f181d
DESKTOP-RJQUL62\aa so <aaronso1200@gmail.com>
2023-08-14 23:40:26 +0800

    Add C# leetcode

---
+++ CSharp/LeetCode/LeetCode/[1]twoSum/twosum.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Net.Security;
+
+namespace LeetCode._1_twoSum
+{
+    public class TwoSumSolution {
+        public int[] TwoSum(int[] nums, int target)
+        {
+            List<int> result = new List<int> {};
+            var storedSum = new Dictionary<int,int>();
+            for (int i = 0 ;i<nums.Length; i++)
+            {
+                int num = nums[i];
+                int substractNum = target - num;
+                if (storedSum.ContainsKey(substractNum))
+                {
+                    result.Add(storedSum[substractNum]);
+                    result.Add(i);
+                    return result.ToArray();
+                }
+
+                storedSum.TryAdd(num,i);
+            }
+
+            return result.ToArray();
+        }
+    }
+}
---
+++ CSharp/LeetCode/LeetCodeUnitTest/[1]twoSum/twoSumTests.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Security.Claims;
+using LeetCode._1_twoSum;
+using NUnit.Framework;
+
+namespace LeetCodeUnitTest
+{
+    [TestFixture]
+    public class TwoSumTest
+    {
+        
+        [Test]
+        [TestCase(new []{1,2,3,4,5},3, new []{0,1})]
+        [TestCase(new []{3,2,4},6, new []{1,2})]
+        [TestCase(new []{1,4,7,1,1,1,1,1},11, new []{1,2})]
+        public void Test1(int[] inputNums, int target, int[] expectResult)
+        {
+            TwoSumSolution twosum = new TwoSumSolution();
+            int[] result = twosum.TwoSum(inputNums,target);
+            CollectionAssert.AreEquivalent(expectResult,result);
+        }
+    }
+}
